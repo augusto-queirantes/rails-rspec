42468e2e7cc69bcb1b30d09b4dfc1d9b07b43ceb
Augusto Queirantes <augusto.queirantes@gmail.com>
2021-06-08 02:13:04 -0300

    Add operation model setup

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,2 +1,9 @@
 class Operation < ApplicationRecord
+  validates :first_number, :second_number, :operation_type, presence: true
+  validates :first_number, :second_number, numericality: { greater_than_or_equal_to: 0 }
+
+  enum operation_type: {
+    plus: 'plus',
+    minus: 'minus'
+  }
 end
