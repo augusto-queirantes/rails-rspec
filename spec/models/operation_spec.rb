df4e5fb67f946338a724e1628bb61810f237d5f7
Augusto Queirantes <augusto.queirantes@gmail.com>
2021-06-08 02:34:15 -0300

    Operation model specs

--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,2 +1,15 @@
 RSpec.describe Operation, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:first_number) }
+    it { is_expected.to validate_presence_of(:second_number) }
+    it { is_expected.to validate_presence_of(:operation_type) }
+    it do
+      is_expected
+        .to define_enum_for(:operation_type)
+        .with_values(
+          plus: 'plus',
+          minus: 'minus'
+        ).backed_by_column_of_type(:string)
+    end
+  end
 end
